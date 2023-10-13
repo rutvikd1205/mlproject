72a913888bac0755c0d98629cb17871ad6ef1671
rutvikd1205 <rutvikdeshpande2017@gmail.com>
2023-10-12 21:44:26 -0400

    Data Ingestion

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     name='mlproject',
     version='0.0.1',
     author='Krish',
-    author_email='krishnaik06@gmail.com',
+    author_email='rutvikdeshpande2017@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
